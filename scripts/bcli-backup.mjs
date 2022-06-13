180e8aae9ecea3d1a94b3fe2ccecbec487276ae5
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-13 06:20:48 +0200

    added timestamp

---
+++ scripts/bcli-backup.mjs
@@ -0,0 +1,42 @@
+import fs from 'fs';
+
+console.log('backing up all files listed in .env file...');
+
+const targetFilename = 'D:\\general-backup\\' + timeStampifyFileName('.env');
+
+fs.copyFile('.env', targetFilename, (err) => {
+	if (err)
+		throw err;
+	console.log('finished');
+});
+
+function getCurrentDateTime() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var month = now.getMonth() + 1;
+	var day = now.getDate();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var second = now.getSeconds();
+	if (month.toString().length == 1) {
+		month = '0' + month;
+	}
+	if (day.toString().length == 1) {
+		day = '0' + day;
+	}
+	if (hour.toString().length == 1) {
+		hour = '0' + hour;
+	}
+	if (minute.toString().length == 1) {
+		minute = '0' + minute;
+	}
+	if (second.toString().length == 1) {
+		second = '0' + second;
+	}
+	var dateTime = year + '-' + month + '-' + day + '-' + hour + '-' + minute + '-' + second;
+	return dateTime;
+}
+
+function timeStampifyFileName(filename) {
+	return `${filename}.backup-${getCurrentDateTime()}`
+}
